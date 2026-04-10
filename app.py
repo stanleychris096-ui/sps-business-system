e5a7a34bd5bad24a74d3a5bc42cce009a5dac3db
stanleychris096-ui <stanley.chris096@gmail.com>
2026-04-10 01:27:23 -0400

    Modify app to read port from environment variable
    Update the application to use a port from environment variable.

--- app.py
+++ app.py
@@ -600,3 +600,8 @@ def inject_today():
 if __name__ == '__main__':
     init_db()
     app.run(debug=True, host='0.0.0.0', port=5000)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
